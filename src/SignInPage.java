629a71e338bac1314f78899cc85cab0bd11dd80a
Antara <antaramimi@gmail.com>
2018-02-22 23:17:46 +0000

    Selenium Practice Code

---
+++ src/SignInPage.java
@@ -0,0 +1,107 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+import java.util.List;
+
+
+public class SignInPage {
+
+
+    public SignInPage clickSignInBtn(WebDriver webDriver) {
+        By signInButton = By.linkText("Sign Up");
+        WebElement SignInButton = webDriver.findElement(signInButton);
+        if (SignInButton.isDisplayed() || SignInButton.isEnabled())
+            SignInButton.click();
+        return new SignInPage();
+    }
+
+
+    private By Username = By.name("uname");
+    private By Password = By.name("pass");
+    private By RepeatPassword = By.name("rpass");
+    private By Email = By.name("email");
+    private By Name = By.name("name");
+
+
+
+
+
+    public void enterUserName(WebDriver webDriver)
+    {
+
+        WebElement SignInUserText = webDriver.findElement(Username);
+        SignInUserText.sendKeys("Ram");
+
+
+        WebElement SignInPasswordText = webDriver.findElement(Password);
+        SignInPasswordText.sendKeys("abc");
+
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(SignInPasswordText).perform();
+        webDriver.findElement(Password).click();
+
+
+        WebElement SignInRPasswordText = webDriver.findElement(RepeatPassword);
+        SignInRPasswordText.sendKeys("abc");
+
+        WebElement SignInEmail = webDriver.findElement(Email);
+        SignInEmail.sendKeys("abc@gmail.com");
+
+        WebElement SignInName = webDriver.findElement(Name);
+        SignInName.sendKeys("ram");
+
+        //WebElement SignInSex = webDriver.findElement(By.id("m"));
+        //SignInSex.click();
+
+        List<WebElement> rad_sex= webDriver.findElements(By.name("gender"));
+        Boolean sex;
+        sex = rad_sex.get(0).isSelected();
+         if(sex==true){
+          rad_sex.get(0).click();
+         }
+         else {
+             rad_sex.get(1).click();
+         }
+
+
+       WebElement Checkbox = webDriver.findElement(By.xpath("//input[@id='tac']"));
+        Checkbox.click();
+
+    }
+
+
+
+
+    public void ErrorforUsername(WebDriver webDriver)
+     {
+         WebElement Errormgsusename= webDriver.findElement(By.xpath("//div[@class='box error radius_3x']"));
+         String Errormg=Errormgsusename.getText();
+         
+         if( Errormgsusename.isEnabled()&& Errormg.contains("Username already taken, please be more creative!"))
+         {
+             System.out.println(Errormg);
+         }
+         else
+
+         {
+           System.out.println("Print the message");
+
+     }
+     }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/test.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver",
+
+                "C:\\Users\\Admin\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.compzets.com/");
+
+        SignInPage S1 = new SignInPage();
+         S1.clickSignInBtn(driver);
+         S1.enterUserName(driver);
+         S1.ErrorforUsername(driver);
+
+    }
+
+
+}
+
